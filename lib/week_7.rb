6e96545c8af78fd7eedf09a75c6a270e42b708fb
Alyssa Colistro <arcolistro@gmail.com>
2019-12-13 11:29:37 -0800

    Tinkering with select() method for third spec.

--- lib/week_7.rb
+++ lib/week_7.rb
@@ -7,14 +7,18 @@ class Anagram
   end
 
   def compare(word1, word2)
-    word1 = word1.downcase.chars.to_set
-    word2 = word2.downcase.chars.to_set
+    word1 = word1.downcase.chars
+    word2 = word2.downcase.chars
 
-    if word1 === word2
+    is_word1 = (word1.select { |i| (i === (i.index-1) & (i.index-2)) })
+    is_word1 = (word1.select { |i| (i === (i.index-1) & (i.index-2)) })
+
+    if (word1.to_set === word2.to_set) & (is_word1 === true) & (is_word2 === true)
       is_anagram = true
     else
       is_anagram = false
     end
     is_anagram
   end
+
 end
